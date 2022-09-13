dbce03f4458917e950ecb7545077a5348122f86d
Monkey <mr.monkey.job@gmail.com>
2022-09-13 17:11:58 +0800

    update grafana

--- examples/main.go
+++ examples/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/jxo-me/gf-metrics"
+	"time"
 )
 
 func main() {
@@ -22,5 +23,25 @@ func main() {
 	m.Use(s)
 	r := s.Group("")
 	r.GET("/hello", func(r *ghttp.Request) { r.Response.Write("Hello") })
+	r.POST("/slow5s", func(r *ghttp.Request) {
+		time.Sleep(time.Duration(5) * time.Second)
+		r.Response.Write("Sleep 5s ok")
+	})
+	r.GET("/slow10s", func(r *ghttp.Request) {
+		time.Sleep(time.Duration(10) * time.Second)
+		r.Response.Write("Sleep 10s ok")
+	})
+	r.POST("/slow20s", func(r *ghttp.Request) {
+		time.Sleep(time.Duration(20) * time.Second)
+		r.Response.Write("Sleep 20s ok")
+	})
+	r.GET("/slow30s", func(r *ghttp.Request) {
+		time.Sleep(time.Duration(30) * time.Second)
+		r.Response.Write("Sleep 30s ok")
+	})
+	r.POST("/slow60s", func(r *ghttp.Request) {
+		time.Sleep(time.Duration(60) * time.Second)
+		r.Response.Write("Sleep 60s ok")
+	})
 	s.Run()
 }
